de1d21435c0e83b57f96221531dab106a83517b3
Felix <afunmibi@gmail.com>
2024-01-20 18:25:12 +0100

    felix-update

---
+++ assets/script/app.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hidesidebar(){
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'none';
+}
