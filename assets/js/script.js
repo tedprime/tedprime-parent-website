c81cbe8e3123258b189827e19d7c7b98ea2c115f
kazo <kazim@bytitude.com>
2024-01-28 12:14:16 +0100

    done

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Navigation
+const ham = document.getElementById('ham')
+const nav = document.getElementById('navul')
+
+ham.addEventListener('click', function(){
+    nav.classList.toggle('active')
+})
+
+//End of Navigation
