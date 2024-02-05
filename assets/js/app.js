ab5fc647446608f77c1c35cbbe9707f1fb3991de
chidubembasil <136812654+chidubembasil@users.noreply.github.com>
2024-02-05 15:32:27 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,39 +3,32 @@ const last = document.getElementById('lname')
 const email = document.getElementById('mail')
 const details = document.getElementById('detail')
 const button = document.getElementById('btn')
-
+const respond1 = document.getElementById('responds1')
 
 
 button.addEventListener('click', function(){
  
-    if(first.value.trim() === ""){
-        alert('Enter your firstname')
-        return 0;
-    }else if(last.value.trim() === ""){
-        alert('Enter your lastname')
-        return 0;
-    }else if(email.value.trim() === ""){
-        alert('Enter your email')
-        return 0;
-    }else if(details.value.trim() === ""){
-        alert('Enter something')
-        return 0;
-    }else{
-        alert('Form submitted successfully!')
-        return 0;
+    if ((first.value.trim() != "") && (last.value.trim() != "") && (email.value.trim() != "") && (details.value.trim() != "")){
+        respond1.innerHTML = "Form is submitted successfully!"
+        respond1.style.fontSize = '1rem'
+        respond1.style.fontWeight = '700'
+        respond1.style.textAlign = 'center'
+        respond1.style.color = '#EF6E11'   
     }
 })
 
 
 const email1 = document.getElementById('input-email')
 const button1 = document.getElementById('bot-btn')
+const respond2 = document.getElementById('responds2')
+
 button1.addEventListener('click', function(){
-    if(email1.value.trim() === ""){
-        alert('Enter your email')
-        return 0;
-    }else{
-        alert('Form submitted successfully!')
-        return 0;
+    if(email1.value.trim() != ""){
+        respond2.innerHTML = "You have been added to our mailing list! "
+        respond2.style.fontSize = '1rem'
+        respond2.style.fontWeight = '700'
+        respond2.style.textAlign = 'center'
+        respond2.style.color = 'white'
     }
 })
 
