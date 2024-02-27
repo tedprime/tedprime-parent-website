45f11bc3139a97bbdfa6120a6196185f33d141ab
okoro chidubem basil <chidbasil@gmail.com>
2024-02-27 15:33:31 +0100

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,30 @@
-const first = document.getElementById('fname')
-const last = document.getElementById('lname')
+const flname = document.getElementById('name')
 const email = document.getElementById('mail')
-const details = document.getElementById('detail')
-const button = document.getElementById('btn')
-const respond1 = document.getElementById('responds1')
+const subject = document.getElementById('subject')
+const message = document.getElementById('textarea')
+const button = document.getElementById('send')
+const respond1 = document.getElementById('respond')
 
 
 button.addEventListener('click', function(){
  
-    if ((first.value.trim() != "") && (last.value.trim() != "") && (email.value.trim() != "") && (details.value.trim() != "")){
+    if ((flname.value.trim() != "") && (email.value.trim() != "") && (subject.value.trim() != "") && (message.value.trim() != "")){
         respond1.innerHTML = "Form is submitted successfully!"
         respond1.style.fontSize = '1rem'
         respond1.style.fontWeight = '700'
         respond1.style.textAlign = 'center'
-        respond1.style.color = '#EF6E11'   
+        respond1.style.color = 'green'   
+
     }
+
 })
 
 
-const email1 = document.getElementById('input-email')
-const button1 = document.getElementById('bot-btn')
+
+
+
+const email1 = document.getElementById('inputtext')
+const button1 = document.getElementById('inputbtn')
 const respond2 = document.getElementById('responds2')
 
 button1.addEventListener('click', function(){
@@ -28,7 +33,7 @@ button1.addEventListener('click', function(){
         respond2.style.fontSize = '1rem'
         respond2.style.fontWeight = '700'
         respond2.style.textAlign = 'center'
-        respond2.style.color = 'white'
+        respond2.style.color = 'green'
     }
 })
 
